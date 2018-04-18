87688ee924ea5937da172a2d86562e652de0041b
christufaro <ctufaro@gmail.com>
2018-04-17 22:02:16 -0400

    Initial Commit and README

---
+++ Xerxes.P2P/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xerxes.P2P
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
