2debc9c8a38ab89b4251596c7bfa6b68d9dd6b76
christufaro <ctufaro@gmail.com>
2018-04-29 13:11:33 -0400

    Rehauling P2P Class, including driver project

--- Xerxes.P2P.Driver/Program.cs
+++ Xerxes.P2P.Driver/Program.cs
@@ -6,9 +6,17 @@ namespace Xerxes.P2P.Driver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");            
-            NetworkPeerServer.StartServer("192.168.1.12",5678); // Start the server  
-            NetworkPeerServer.Listen(); // Start listening. 
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Please specify server xxx.xxx.xxx.xxx and port xxxx in the command line.");
+            }
+            else
+            {
+                string server = args[0];
+                int port = Int32.Parse(args[1]);
+                NetworkPeerServer.StartServer("192.168.1.12", 5678); // Start the server  
+                NetworkPeerServer.Listen(); // Start listening. 
+            }
         }
     }
 }
